7e2cf4aa5d4209d2680b4f8b6ad6a9afb37069d6
Eddie Moy <emoy@tulane.edu>
2021-10-30 22:45:17 +0000

    test

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'home/index'
   root to: "home#index"
   resources :organizations
-  root :to => redirect('/organizations')
+  get 'home/index', to: 'static_pages#home', as: 'home'
   
 end
