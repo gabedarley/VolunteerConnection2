44a9db0a3b96dc0c2f6972a994e3afb08c946863
Eddie Moy <emoy@tulane.edu>
2021-10-30 22:56:57 +0000

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'home/index'
   root to: "home#index"
-  resources :organizations
+  resources :organizations, :events
   get 'home/index', to: 'static_pages#home', as: 'home'
   
 end
