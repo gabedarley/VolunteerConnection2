4dd95f8d7aa7ef53bd31f9d93a6862a82fd21912
Eddie Moy <emoy@tulane.edu>
2021-11-02 12:26:28 +0000

    Dropped users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20211101231358) do
+ActiveRecord::Schema.define(version: 20211102121922) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -32,10 +32,20 @@ ActiveRecord::Schema.define(version: 20211101231358) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password"
-    t.string "uid"
-    t.string "provider"
+    t.string   "username"
+    t.string   "password"
+    t.string   "uid"
+    t.string   "provider"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string   "name"
+    t.string   "lastname"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
