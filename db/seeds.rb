38957baadabdc1c12e44ec955fba1dbe1ea97536
Chloe Uhls <cuhls@tulane.edu>
2021-11-03 20:34:30 +0000

    added 20 entries to users table

--- db/seeds.rb
+++ db/seeds.rb
@@ -110,11 +110,65 @@ end
 
 users = [
    {
-    :username => 'chloeu99',
-    :password => 'ilikedogs'}
+    :email => 'cuhls@tulane.edu',
+    :encrypted_password => 'ilikedogs'},
    {
-    :username => 'gabedarley',
-    :password => 'password'}
+    :email => 'gdarley@tulane.edu',
+    :encrypted_password => 'password123'},
+   {
+    :email => 'emoy@tulane.edu',
+    :encrypted_password 'password302'=> },
+   {
+    :email => 'skellum@tulane.edu',
+    :encrypted_password => 'password239'},
+   {
+    :email => 'akurdia@tulane.edu',
+    :encrypted_password => 'password873'},
+   {
+    :email => 'sbroth@tulane.edu',
+    :encrypted_password => 'password888'},
+   {
+    :email => 'obulter@tulane.edu',
+    :encrypted_password => 'password399'},
+   {
+    :email => 'ocanosa@tulane.edu',
+    :encrypted_password => 'password033'},
+   {
+    :email => 'adonahue@tulane.edu',
+    :encrypted_password => 'password328'},
+   {
+    :email => 'bgarner@tulane.edu',
+    :encrypted_password => 'password022'},
+   {
+    :email => 'ggilliom@tulane.edu',
+    :encrypted_password => 'password477'},
+   {
+    :email => 'bkahn@tulane.edu',
+    :encrypted_password => 'password832'},
+   {
+    :email => 'jlehner@tulane.edu',
+    :encrypted_password => 'password473'},
+   {
+    :email => 'cmaira@tulane.edu',
+    :encrypted_password => 'password678'},
+   {
+    :email => 'mmotz@tulane.edu',
+    :encrypted_password => 'password002'},
+   {
+    :email => 'econnell@tulane.edu',
+    :encrypted_password => 'password453'},
+   {
+    :email => 'kromer@tulane.edu',
+    :encrypted_password => 'password193'},
+   {
+    :email => 'msender@tulane.edu',
+    :encrypted_password => 'password772'},
+   {
+    :email => 'yzhang@tulane.edu',
+    :encrypted_password => 'password892'},
+   {
+    :email => 'cmayberry@tulane.edu',
+    :encrypted_password => 'password444'},
     
 ]
     
