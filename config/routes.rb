712cb1a593b8e41c02289cc13685820c4c6a5d92
Eddie Moy <emoy@tulane.edu>
2021-11-03 12:50:44 +0000

    Fixed login, logout on local

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   get 'home/index', to: 'static_pages#home', as: 'home'
 #   get ‘auth/:provider/callback’, to: ‘sessions#googleAuth’
 #   get ‘auth/failure’, to: redirect(‘/’)
-  
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 
 
 end
