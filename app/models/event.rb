e819bf903ceea5bc3e9e3d04a2a27240f188f32e
Gabe Darley <gdarley@tulane.edu>
2021-11-04 05:27:27 +0000

    Added model associations

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,6 @@ class Event < ActiveRecord::Base
   validates :date, :presence => true #:format => { :with => /\A(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}\z|\A\z/}
   validates :start_time, :format => { :with => /\A([0-1]?[0-9]|2[0-3]):[0-5][0-9]\z|\A\z/}
   validates :end_time, :format => { :with => /\A([0-1]?[0-9]|2[0-3]):[0-5][0-9]\z|\A\z/}
+    
+  belongs_to :organization
 end
