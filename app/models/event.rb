c2157de121b8a920ff21203e34ee75ddf9fa5f03
Eddie Moy <emoy@tulane.edu>
2021-11-15 23:24:15 +0000

    added validation to the organization attributes

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,4 +6,5 @@ class Event < ActiveRecord::Base
   validates :end_time, :format => { :with => /\A([0-1]?[0-9]|2[0-3]):[0-5][0-9]\z|\A\z/}
     
   belongs_to :organization
+  validates :organization, :presence => true
 end
