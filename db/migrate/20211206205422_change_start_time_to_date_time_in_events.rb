189b59918d392e73f7182a710928813290512174
Gabe Darley <gdarley@tulane.edu>
2021-12-07 00:25:39 +0000

    Fixing datetime error

--- db/migrate/20211206205422_change_start_time_to_date_time_in_events.rb
+++ db/migrate/20211206205422_change_start_time_to_date_time_in_events.rb
@@ -1,6 +1,6 @@
 class ChangeStartTimeToDateTimeInEvents < ActiveRecord::Migration
   def change
-      change_column :events, :start_time, :datetime
-      change_column :events, :end_time, :datetime
+      change_column :events, :start_time, :datetime, "USING start_time::timestamp without time zone"
+      change_column :events, :end_time, :datetime, "USING start_time::timestamp without time zone"
   end
 end
