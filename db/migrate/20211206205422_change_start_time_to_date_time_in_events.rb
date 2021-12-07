046f9a3b905f39fc31a08339b48c1d34292d8134
Gabe Darley <gdarley@tulane.edu>
2021-12-07 00:34:36 +0000

    Fixing datetime error

--- db/migrate/20211206205422_change_start_time_to_date_time_in_events.rb
+++ db/migrate/20211206205422_change_start_time_to_date_time_in_events.rb
@@ -2,5 +2,34 @@ class ChangeStartTimeToDateTimeInEvents < ActiveRecord::Migration
   def change
       change_column :events, :start_time, :datetime, "USING start_time::timestamp without time zone"
       change_column :events, :end_time, :datetime, "USING start_time::timestamp without time zone"
+      
+      
+    # add a temporary column
+    add_column :events, :start_time_datetime, :datetime
+    
+    # add the the current start_time as datetime to the temporary column for each entry
+    Event.all.each do |event|
+      event.update(start_time_datetime: assignment.start_time.to_datetime)
+    end
+
+    # drop the old time column
+    remove_column :events, :start_time
+    
+    # rename the temporary column to start_time   
+    rename_column :events, :start_time_datetime, :start_time
+      
+    # add a temporary column
+    add_column :events, :end_time_datetime, :datetime
+    
+    # add the the current start_time as datetime to the temporary column for each entry
+    Event.all.each do |event|
+      event.update(end_time_datetime: event.end_time.to_datetime)
+    end
+
+    # drop the old time column
+    remove_column :events, :end_time
+    
+    # rename the temporary column to start_time   
+    rename_column :events, :end_time_datetime, :end_time
   end
 end
